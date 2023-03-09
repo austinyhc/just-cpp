4de452781a6095360d60effef355e809a604134f
Austin Chen <austin.yhc@gmail.com>
2023-03-09 14:39:50 +0800

    🧠 Add leetcode 125.

---
+++ src/0125_valid_palindrome.h
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <cctype>
+#include <vector>
+#include "utils/base.h"
+
+class Solution {
+public:
+    bool _isPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        while (i < j) {
+            while (i < j && !isalnum(s[i])) ++i;
+            while (i < j && !isalnum(s[j])) --j;
+            
+            if (tolower(s[i++]) != tolower(s[j--]))
+                return false;
+        }
+        return true;
+    }
+
+    // Recursive solution if the stack size is permitted
+    bool isPalindrome(string s) {
+        int lo = 0;
+        int hi = s.size() - 1;
+        return isPalindrome(s, lo, hi);
+    }
+
+    bool isPalindrome(string const& s, int lo, int hi) {
+
+        while (!isalnum(s[lo])) ++lo;
+        while (!isalnum(s[hi])) --hi;
+        
+        if (lo >= hi) { return true; }
+
+        if (tolower(s[lo++]) == tolower(s[hi--])) {
+            return isPalindrome(s, lo, hi);
+        }
+        return false;
+    }
+};
---
+++ test/0125_valid_palindrome_test.cc
@@ -0,0 +1,13 @@
+#include "0125_valid_palindrome.h"
+
+TEST(S, Demo) {
+    Solution s;
+    EXPECT_EQ(s.isPalindrome("A man, a plan, a canal: Panama"),
+              s._isPalindrome("A man, a plan, a canal: Panama"));
+    EXPECT_EQ(s.isPalindrome("race a car"),
+              s._isPalindrome("race a car"));
+    EXPECT_EQ(s.isPalindrome(" "),
+              s._isPalindrome(" "));
+    EXPECT_EQ(s.isPalindrome(".,"),
+              s._isPalindrome(".,"));
+}

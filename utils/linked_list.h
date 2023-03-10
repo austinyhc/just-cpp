2c7407de877cced61ac3c6318523db463d394d98
Austin Chen <austin.yhc@gmail.com>
2023-03-10 11:12:25 +0800

    🧱 Modularize linked_list.h

---
+++ utils/linked_list.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <cstddef>
+#include <initializer_list>
+#include <vector>
+
+namespace leetlib {
+
+template <typename T>
+struct ListNode {
+    T val;
+    ListNode* next;
+    ListNode() : next(NULL) {}
+    ListNode(T x) : val(x), next(NULL) {}
+
+    static ListNode* Factory(const std::initializer_list<T> l) {
+        if (l.size() == 0) return nullptr;
+        auto iter = l.begin();
+        ListNode* cur = new ListNode(*iter);
+        ListNode* ret = cur;
+        ++iter;
+        while (iter != l.end()) {
+            cur->next = new ListNode(*iter);
+            cur = cur->next;
+            ++iter;
+        }
+        return ret;
+    }
+
+    static ListNode* Factory(const std::vector<T> l) {
+        if (l.size() == 0) return nullptr;
+        auto iter = l.begin();
+        ListNode* cur = new ListNode(*iter);
+        ListNode* ret = cur;
+        ++iter;
+        while (iter != l.end()) {
+            cur->next = new ListNode(*iter);
+            cur = cur->next;
+            ++iter;
+        }
+        return ret;
+    }
+
+    static std::vector<T> Dump(ListNode* head) {
+        std::vector<T> ret;
+        while (head) {
+            ret.push_back(head->val);
+            head = head->next;
+        }
+
+        return ret;
+    }
+};
+
+template<typename T>
+struct BiDirListNode {
+    T val;
+    BiDirListNode* prev;
+    BiDirListNode* next;
+    BiDirListNode() : prev(NULL), next(NULL) {}
+    BiDirListNode(T x) : val(x), prev(NULL), next(NULL) {}
+};
+
+} // namespace leetlib
